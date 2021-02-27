ce7e22450792dd276fe1c85bba545a31ffa86623
akosimarksalvador <markanthonybuhaysalvador@gmail.com>
2021-02-27 14:00:17 +0800

    google search fix

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -6,7 +6,7 @@ export const environment = {
   apiUrl: "https://marcsalvador.github.io/angular-material-reactive-forms/assets/mockdata/",
   appUrl: "https://marcsalvador.github.io/angular-material-reactive-forms/",
   package: require('../../package.json'),
-  googleBrowserKey: '[]',
+  googleBrowserKey:  '',
   paypal: {
     mode: 'production',
     sandboxClientId: 'AW35MxsELLQji9DqLDeEK0wd7O1CjeBIVdlE1xs4p0VNQtaC9h-xzDY_lZFEm-4lmxQ-C6sT9uvCbiqr',
